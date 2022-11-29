4507b5a566e80ee2d27ad6e3a30c39d8249efdee
eSTee3 <tagsxa@yahoo.com>
2022-11-29 17:28:22 -0600

    Additional TODO items

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,3 @@
 // TODO: Create api call to pull in data from https://openweathermap.org/forecast5
 // TODO: Pull information from the end user's inputs, to query the api
+// TODO: Save user's search history on the landing page
